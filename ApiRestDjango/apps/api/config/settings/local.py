f324f2937b0977285e1dd3f5ba66b11b361838ed
Diego Ladino <galatas96@gmail.com>
2025-03-08 11:04:50 -0500

    Add initial project structure and configuration files for Django API

---
+++ ApiRestDjango/apps/api/config/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "api_django_db",
+        "USER": "root",
+        "PASSWORD": "123456",
+        "HOST": "postgres",
+        "PORT": "5432",
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
