a2ee5173b90b2e7a9282c4a6b82a7d5903036042
Diego Ladino <galatas96@gmail.com>
2025-03-09 19:21:51 -0500

    Modify encryption service: update key management to use settings, improve password verification method, and enhance code readability

--- ApiRestDjango/apps/api/modules/shared/utilities/encryption.py
+++ ApiRestDjango/apps/api/modules/shared/utilities/encryption.py
@@ -1,19 +1,28 @@
 from cryptography.fernet import Fernet
+from django.conf import settings
 
 # Generar una clave de cifrado
-new_key = Fernet.generate_key()
-ciphered = Fernet(new_key)
+# new_key = Fernet.generate_key()
+key = settings.ENCRYPTION_KEY.encode()
+ciphered = Fernet(key)
+print(key)
 
 
 class EncryptionService:
     @staticmethod
-    def encrypt_password(raw_password: str) -> bytes:
-        return ciphered.encrypt(raw_password.encode())
+    def encrypt_password(password: str) -> bytes:
+        return ciphered.encrypt(password.encode())
 
     @staticmethod
     def decrypt_password(encrypted_password: bytes) -> str:
         return ciphered.decrypt(encrypted_password).decode()
 
     @staticmethod
-    def verify_password(raw_password: str, encrypted_password: bytes) -> bool:
-        return EncryptionService.decrypt_password(encrypted_password) == raw_password
+    def verify_password(password: str, encrypted_password: bytes) -> bool:
+        try:
+            decrypted_password = EncryptionService.decrypt_password(
+                encrypted_password
+            )
+            return decrypted_password == password
+        except Exception:
+            return False
